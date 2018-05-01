83c87b220a23ead1777b615bf94d481c5dcccde4
Blaise James <blaise@blaisejames.com>
2018-05-01 18:17:02 -0400

    secon dcommit of findCharacters.py

--- findCharacters.py
+++ findCharacters.py
@@ -2,9 +2,9 @@
 word_list = ['hello','world','my','name','is','Anna']
 char = 'o'
 # output
-new_list = ['hello','world']
+new_list = []
 
-if char in word_list: 
-    new_list.append()
-
+for word in word_list:
+    if char in word:
+        new_list.append(word)
 print new_list
