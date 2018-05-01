0a025413cf51332a6133f33d37b6884dd234ae38
Blaise James <blaise@blaisejames.com>
2018-05-01 17:52:09 -0400

    first commit of findCharacters.py

---
+++ findCharacters.py
@@ -0,0 +1,10 @@
+# input
+word_list = ['hello','world','my','name','is','Anna']
+char = 'o'
+# output
+new_list = ['hello','world']
+
+if char in word_list: 
+    new_list.append()
+
+print new_list
